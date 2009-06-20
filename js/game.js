a848a8f57df03c5026549352800f13ceede28c0f
Brendan Fahy <brendan@f4hy.com>
2009-06-20 02:03:06 -0700

    Now sets all moves for selected piece.

--- js/game.js
+++ js/game.js
@@ -15,43 +15,23 @@ $(document).ready(function () {
     function setboard(json) {
 	var boardState = eval('(' + json + ')');
 	var pieces = boardState["positions"];
-	moves = boardState["moves"];
-	for (x in pieces){
+	moves = boardState.moves;
+	for (x in pieces) {
 	    addPiece(pieces[x],"",x[0],x[2]);
 	}
 	draginit();
     }
 
 
-    
-    function parsemoves(json) {
-	myMoves = eval('(' + json + ')');
-	// for (move in myMoves){
-	//     getSquare(move[0],move[2]).css({background:"orange"});
-	// }
-	seven = 8;
-	return false;
-    }
-
     var board = $("#board");
-    for(i=0;i<10;i+=1){
-	for(j=0;j<9;j+=1){
+    for(i=0;i<10;i+=1) {
+	for(j=0;j<9;j+=1) {
 	    s = "<div id=square" + i + "" + j + " class='square'></div>";
 	    $(s).appendTo(board).css("left",(70*j)+"px").css("top",(70*i)+"px");
 	    
 	}
     }
     
-    //    $(".square").each(function (i) {
-    //$(this).text($(this).data("index").x + "," + $(this).data("index").y);
-    //   });
-
-    // $.getJSON("moves.cgi", parsemoves );
-
-    // function test() {
-    // 	seven = 4;
-    // }
-
     // $("body").append(myMoves);
     // test();
     // $("body").append("test");
@@ -75,8 +55,11 @@ $(document).ready(function () {
 	    // $("body").append( "omg");
 
 	    $(".square").droppable('disable');
-	    var tmp = $(this).data("moves")
-	    getSquare(tmp[0][0],tmp[0][1]).droppable('enable');
+	    var selectedMoves = $(this).data("moves")
+	    for(i=0; i< selectedMoves.length; i++) {
+		    getSquare(selectedMoves[i][0],selectedMoves[i][1]).droppable('enable');
+	    }
+
 	    if( $(this).hasClass('rook') ) {
 		$(".square").droppable('disable');
 		getSquare(1,2).droppable('enable');
